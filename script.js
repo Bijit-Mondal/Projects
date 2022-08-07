e2070475512ebf4d07603ce7f96f1c569c1b38f3
Bijit Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2022-08-07 08:06:30 +0530

    Update Script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const username = '2kabhishek';
+const username = 'Bijit-Mondal';
 const maxPages = 2;
 const repoList = document.querySelector('.repo-list');
 const reposSection = document.querySelector('.repos');
